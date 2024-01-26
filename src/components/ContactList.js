0ac204f0ad443ff0b7d8d0f7f93407bac47bbb99
Harshit Shukla <mrharshit2409@gmail.com>
2024-01-26 21:23:24 +0530

    added users avatar

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ContactCard from "./ContactCard"
 
 const ContactList = (props) => {
     console.log(props);
@@ -10,15 +11,7 @@ const ContactList = (props) => {
 
     const renderContactList = props.contacts.map((contact) => {
         return (
-            <div className="item" key={contact.id}>
-                <div className="content">
-                    <div className="header">{contact.name}</div>
-                    <div>{contact.email}</div>
-                    <div>
-                        <i className="trash alternate outline icon"></i>
-                    </div>
-                </div>
-            </div>
+            <ContactCard contact = {contact}></ContactCard>
         );
     });
 
